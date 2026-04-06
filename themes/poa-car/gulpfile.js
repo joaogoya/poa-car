c3a61ab2dcae1948c3317e59638f52f35dddc3af
Joao <joaogoya@gmail.com>
2026-04-06 16:47:56 -0300

    Configs Iniciais HTML Basico e css no header

---
+++ themes/poa-car/gulpfile.js
@@ -0,0 +1,117 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass')); 
+const sourcemaps = require('gulp-sourcemaps');   
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const autoprefixer = require('autoprefixer');
+const concat = require('gulp-concat');
+const postcss = require('gulp-postcss');
+
+const paths = {
+  styles: {
+    vendors: [
+      './assets/css/bootstrap.min.css',
+      './assets/css/animate.min.css',
+      './assets/css/font-awesome.min.css',
+      './assets/css/pe-icon-7-stroke.css',
+      './assets/css/magnific-popup.css',
+      './assets/css/slick.css',
+      './assets/css/meanmenu.min.css'
+    ],
+   
+    src: [
+      './assets/scss/_variables.scss', 
+      './assets/scss/_mixin.scss',    
+      './assets/scss/_common.scss',    
+      './assets/scss/_header.scss',
+      './assets/scss/_banner.scss',
+      './assets/scss/_about.scss',
+      './assets/scss/_services.scss',
+      './assets/scss/_cta.scss',
+      './assets/scss/_features.scss',
+      './assets/scss/_video.scss',
+      './assets/scss/_team.scss',
+      './assets/scss/_skill.scss',
+      './assets/scss/_portfolio.scss',
+      './assets/scss/_testimonial.scss',
+      './assets/scss/_blog.scss',
+      './assets/scss/_contact.scss',
+      './assets/scss/_framework.scss',
+      './assets/scss/_counter.scss',
+      './assets/scss/_breadcrumb.scss',
+      './assets/scss/_sidebar.scss',
+      './assets/scss/_faq.scss',
+      './assets/scss/_footer.scss'
+    ],
+    watch: './assets/scss/**/*.scss',
+    dest: './assets/dist/'
+  },
+scripts: {
+    src: [
+      // 1. BASE: Força a entrada do jQuery e Modernizr (Coringas no nome)
+      'assets/js/vendor/jquery*.js', 
+      'assets/js/vendor/modernizr*.js',
+
+      // 2. DEPENDÊNCIAS: Popper e Bootstrap
+      'assets/js/popper*.js',
+      'assets/js/bootstrap*.js',
+
+      // 3. PLUGINS: Todos os plugins do template
+      'assets/js/slick*.js',
+      'assets/js/isotope*.js',
+      'assets/js/jquery.magnific-popup*.js',
+      'assets/js/jquery.inview*.js',
+      'assets/js/jquery.countTo*.js',
+      'assets/js/jquery.easypiechart*.js',
+      'assets/js/jquery.meanmenu*.js',
+
+      // 4. LÓGICA DO TEMA: Seus arquivos finais
+      'assets/js/main.js',
+      'assets/js/template_sctipt.js', 
+
+      // 5. EXCLUSÕES INTELIGENTES
+      '!assets/js/vendor/jquery-migrate*', 
+      '!assets/dist/**'
+    ],
+    dest: 'assets/dist/'
+  }
+};
+
+function buildCss() {
+  // O Gulp vai ler todos os arquivos da lista na ordem
+  return gulp.src([...paths.styles.vendors, ...paths.styles.src])
+    .pipe(sourcemaps.init())
+    .pipe(concat('style.min.css')) // Junta tudo primeiro (desencapsula)
+    .pipe(sass({
+        quietDeps: true // Silencia avisos de @import se houver algum nos vendors
+    }).on('error', sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(cleanCSS({ level: 2, compatibility: 'ie8' }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+function scripts() {
+  // Nota: allowEmpty: true ajuda a não quebrar se um plugin opcional sumir
+  return gulp.src(paths.scripts.src, { allowEmpty: true })
+    .pipe(sourcemaps.init())
+    .pipe(concat('scripts.min.js'))
+    .pipe(uglify().on('error', function (err) {
+        console.error('Erro no JS:', err.toString());
+        this.emit('end');
+    }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+function watchFiles() {
+  gulp.watch(paths.styles.watch, buildCss);
+  // Observa mudanças em qualquer JS para refazer o bundle
+  gulp.watch(['assets/js/**/*.js', '!assets/dist/**'], scripts);
+}
+
+exports.buildCss = buildCss;
+exports.scripts = scripts;
+
+const build = gulp.series(gulp.parallel(buildCss, scripts), watchFiles);
+exports.default = build;
---
+++ themes/poa-car/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/telas/css/comon.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/telas/css/hero.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/telas/css/sobre.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/telas/css/revisoes.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/telas/css/logo.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/telas/css/service.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/telas/css/testimonials.css">
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/telas/css/oam.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/telas/css/form.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/telas/css/footer.css">
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Oswald:wght@500;700&family=Roboto:wght@300;400;700&display=swap"
+        rel="stylesheet">
+
+
+
+
+
+    <?php wp_head(); ?>
+</head>
+
+<body >
